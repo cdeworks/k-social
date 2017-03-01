6ad2d35221aa2c2870977e66e0786a5bf608e02a
Daniel <cdeworksbr@gmail.com>
2017-03-01 08:32:22 -0300

    primeiro commit

---
+++ src/main/java/com/cdeworks/ksocial/KsocialApplication.java
@@ -0,0 +1,15 @@
+package com.cdeworks.ksocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsocialApplication.class, args);
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/cdeworks/ksocial/KsocialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cdeworks.ksocial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KsocialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
