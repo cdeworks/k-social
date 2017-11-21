a0eab09be9088315576fe78f01e165a1862ee4e1
Daniel <danielcdeworks@gmail.com>
2017-11-21 15:29:52 -0200

    Configurações gerais de usuarios e roles

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into ksocial_db.associado (id, data_alteracao, data_criacao, data_nascimento, email, cep, cidade, complemento, estado, logradouro, numero, tipo_endereco, nome, senha) VALUES(0, null, null, null, 'dreis2003@hotmail.com', '07061032', 'Guarulhos', 'Bloco 4 apt 124', 'SP', 'Av da Paz', '209', 'AVENIDA', 'Daniel Reis', '$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.');
+
+insert into role ('ROLE_ADMIN');
+insert into role ('ROLE_USER');
+insert into associado_roles values (1,'ROLE_ADMIN');
